a65c7b5aa1c80b3a279519a2ee8d406f1784d376
Gennadiy Civil <misterg@google.com>
2019-08-07 13:56:53 -0400

    WIP

--- ci/build-linux-bazel.sh
+++ ci/build-linux-bazel.sh
@@ -10,3 +10,5 @@ bazel test --curses=no //...:all
 bazel test @com_google_absl//absl/...:all --test_tag_filters=-benchmark
 
 bazel test @com_google_googletest//googletest/...:all --define absl=1
+
+bazel test @com_github_google_benchmark//test/...:all
